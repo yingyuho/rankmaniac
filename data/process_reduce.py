4f4f3b8c220ef33c4a73157ae5d8949e014282d7
yingyu <heinsolid@gmail.com>
2014-02-06 21:35:13 -0800

    processing reducer

--- data/process_reduce.py
+++ data/process_reduce.py
@@ -2,10 +2,28 @@
 
 import sys
 
-#
-# This program simply represents the identity function.
-#
+prevNode = None
+rCur = rPrev = 0.0
+outNode = ''
+finalRank = []
 
 for line in sys.stdin:
-    sys.stdout.write(line)
+    info = line.split("\t")
+    nodeid = info[0]
+    attr = info[1].rstrip('\n').split(",")
 
+    if nodeid == 'F':
+        finalRank.append( (attr[0], float(attr[1]), float(attr[2])) )
+        continue
+
+    if len(attr) == 2:
+        rCur = attr[0]
+        rPrev = attr[1]
+    else:
+        outNode += ',%s' % attr[0]
+
+    if nodeid != prevNode:
+        if prevNode != None:
+            sys.stdout.write('NodeId:%s\t%s,%s%s\n' % (prevNode, rCur, rPrev, outNode))
+            outNode = ''
+        prevNode = nodeid
