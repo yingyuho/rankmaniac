ebaa5e872f30a35af6369f38f7c06fb6f49ce2f8
yingyu <heinsolid@gmail.com>
2014-02-06 22:00:13 -0800

    fix a bug that PR were shifted to other node

--- data/process_reduce.py
+++ data/process_reduce.py
@@ -16,17 +16,18 @@ for line in sys.stdin:
         finalRank.append( (attr[0], float(attr[1]), float(attr[2])) )
         continue
 
+    if nodeid != prevNode:
+        if prevNode != None:
+            sys.stdout.write('NodeId:%s\t%s,%s%s\n' % (prevNode, rCur, rPrev, outNode))
+            outNode = ''
+        prevNode = nodeid
+
     if len(attr) == 2:
         rCur = attr[0]
         rPrev = attr[1]
     else:
         outNode += ',%s' % attr[0]
 
-    if nodeid != prevNode:
-        if prevNode != None:
-            sys.stdout.write('NodeId:%s\t%s,%s%s\n' % (prevNode, rCur, rPrev, outNode))
-            outNode = ''
-        prevNode = nodeid
 
 sys.stdout.write('NodeId:%s\t%s,%s%s\n' % (prevNode, rCur, rPrev, outNode))
 outNode = ''
