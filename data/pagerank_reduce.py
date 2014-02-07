724e0184bf785abbada18eaeaf4c4c602859d761
Kexin Rong <kexinrongg@gmail.com>
2014-02-06 20:48:00 -0800

    Implemented pagerank_reduce
    - Input: node_id\t(n_id, n_cpr / n_deg)\n or
             node_id\t(node_id, ppr)\n
    - Output: node_id \t cpr, ppr, in_node1, in_node2, .... \n

--- data/pagerank_reduce.py
+++ data/pagerank_reduce.py
@@ -2,10 +2,58 @@
 
 import sys
 
-#
-# This program simply represents the identity function.
-#
+# Helper function to calculate current page rank, and format output string
+# Output format:
+#	node_id \t cpr, ppr, in_node1, in_node2, .... \n
+def format_output():
+    cpr = 1 - alpha
+    for node in neighbours:
+        cpr += node[1]
 
+    strout = str(node_id) + '\t' + str(cpr) + ',' + str(ppr)
+    for node in neighbours:
+        strout = strout + ',' + str(node[0])
+    strout += '\n'
+    sys.stdout.write(strout)
+
+#
+# stdin format:
+#       node_id\t(n_id, n_cpr / n_deg)\n
+#   or node_id\t(node_id, ppr)\n
+#   
+# To distinguish between the cases, ppr is always negative.
+
+alpha = 0.85
+ppr = 0
+node_id = -1
+neighbours = []
 for line in sys.stdin:
-    sys.stdout.write(line)
+    # Extract current node_id
+    line = line.strip()
+    pos = line.find('\t')
+    id = int(line[:pos])
+    # Get rid of the parentheses
+    line = line[pos + 2 : -1]
+
+    # When we finish collecting information on one node
+    if id != node_id:
+        # Calculate new page rank and output
+        if node_id != -1:
+            format_output()
+            
+        node_id = id
+        neighbours = []
+
+    # Collect its neighbour's node id and cpr/deg
+    pos = line.find(',')
+    id = int(line[:pos])
+    val = float(line[pos + 1:])
+    if val < 0 :
+        ppr = -val
+    else:
+        neighbours.append([id, val])
+        
+
+# Print last node
+format_output()
 
