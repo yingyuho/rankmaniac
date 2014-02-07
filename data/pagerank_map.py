6f61b4df39962d691f35379d8ccd8daa85d8d7d1
unknown <jchen2@caltech.edu>
2014-02-06 18:51:16 -0800

    Parser added along with test scripts

--- data/pagerank_map.py
+++ data/pagerank_map.py
@@ -7,4 +7,13 @@ import sys
 #
 
 for line in sys.stdin:
-    sys.stdout.write(line)
+    info = (line.split(":")[1]).split("\t")
+    nodeid = int(info[0])
+    attributes = (info[1]).split(",")
+    (currrank, prevrank) = (float(attributes[0]), float(attributes[1]))
+    neighbours = attributes[2:]
+    degree = len(neighbours)
+    profile = (nodeid, currrank, degree)
+    for node in neighbours:
+        sys.stdout.write(node + "\t" + str(profile))
+    sys.stdout.write(str(nodeid) + "\t" + str(profile))
