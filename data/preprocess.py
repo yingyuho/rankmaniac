857859571a721492a980bad9bfbb1b5b5275e3e1
yingyu <heinsolid@gmail.com>
2014-02-09 18:41:05 -0800

    changed format of data set preprocessor to match pagerank_map

--- data/preprocess.py
+++ data/preprocess.py
@@ -15,6 +15,6 @@ def read_input(f):
 
 for key, group in groupby(read_input(sys.stdin), itemgetter(0)):
     # group: iterator to all data (key included) sharing this key
-    sys.stdout.write('N:%s\t1.0,1.0' % (key, ))
+    sys.stdout.write('NodeId:%s\t1.0,0.0' % (key, ))
     sys.stdout.write(''.join([',%s' % attr for attr in imap(itemgetter(1), group)]))
     sys.stdout.write('\n')
