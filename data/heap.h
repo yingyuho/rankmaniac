0d5e81b08225262746b498245df198c0cf980233
unknown <jchen2@caltech.edu>
2014-02-11 18:20:14 -0800

    Implemented a pageRank based min-heap

---
+++ data/heap.h
@@ -0,0 +1,37 @@
+#ifndef __HEAP_H__
+#define __HEAP_H__
+
+/* Maximum number of values that can be stored in the heap. */
+#define MAX_HEAP_ELEMS 50
+
+/*
+ * A simple heap data structure, for storing floats.
+ */
+typedef struct
+{
+  int node_id;
+  float cpr;
+  float ppr;
+} pageRank;
+ 
+typedef struct
+{
+  /* Number of values currently in the heap. */
+  int num_values;
+
+  /* The values in the heap. */
+  pageRank values[MAX_HEAP_ELEMS];
+} float_heap;
+
+
+/* Initialize a heap data structure. */
+void init_heap(float_heap *pHeap);
+
+/* Returns the first (i.e. smallest) value in the heap. */
+float get_first_value(float_heap *pHeap);
+
+/* Adds another value to the heap, in the proper place. */
+void add_value(float_heap *pHeap, float newval);
+
+#endif /* __HEAP_H__ */
+
